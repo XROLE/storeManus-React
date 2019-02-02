a1d7fd87d7b8818ada6bd7cb51891268c84b0150
xrole <xrolediamond@gmail.com>
2019-02-02 14:14:26 +0100

    [featrue: #163671334] Setup empty react project install react and react-dom install and configure webpack v4 install and configure babel to transpile js and jsx configure gitignore to ignore node_modules setup a simple react compoent to test functinality [Finishes #163671334]

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return <div>
+      <h1> StoreManus React Version</h1>
+    </div>
+  }
+}
+
+export default App;
