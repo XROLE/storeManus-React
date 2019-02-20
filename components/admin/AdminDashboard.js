56f2ac1edc66535b97e9272f56e1fdee2ecfeee3
xrole <xrolediamond@gmail.com>
2019-02-20 09:12:29 +0100

    resolve git issue

--- components/admin/AdminDashboard.js
+++ components/admin/AdminDashboard.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import Navbar from '../navbar/Navbar';
+import Navbar from '../navbar/tyu';
 
 export default class AdminDashboard extends Component {
   constructor() {
