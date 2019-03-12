ee035176e1322bc91d8083f1a66076236893a003
xrole <xrolediamond@gmail.com>
2019-03-12 11:02:56 +0100

    [chore: #164564974] deploy to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,48 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'src'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
-      },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+          ],
+        },
+      ],
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.join(__dirname, 'src'),
+      hot: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin(
+        {
+          template: './src/index.html',
+        },
+      ),
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin(
-      {
-        template: './src/index.html',
-      },
-    ),
-  ],
+  };
 };
